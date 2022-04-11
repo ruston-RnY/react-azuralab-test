0ca65d208f4c39385e6c23b1289ed9a3aaf75a49
ruston.efendi <ruston.efendi@redtech.co.id>
2022-04-11 14:31:47 +0700

    setup project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div>
-      <p>halo</p>
+      <div className="container">
+        <p>halo</p>
+        <button className='btn btn-primary'>hay</button>
+
+      </div>
     </div>
   );
 }
