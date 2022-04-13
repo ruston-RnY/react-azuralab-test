34e3cee99a54f677980c1c4797416468b2a93ece
ruston.efendi <ruston.efendi@redtech.co.id>
2022-04-13 11:12:33 +0700

    fix responsive contact

--- src/parts/contact/Contact.js
+++ src/parts/contact/Contact.js
@@ -8,25 +8,25 @@ export default function Contact(props) {
   const className = ["contact",props.className]
   return (
     <section className={className.join(" ")}>
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-            <div className="contact-wrap d-flex justify-content-between">
-              <div className="contact-content">
-                <h3>layanan berkualitas tinggi <br /> untuk masyarakat indonesia</h3>
-                <p className='py-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-                <Button isLargeColored className="mr-3">
-                  Kontak Kami
-                </Button>
-                <Button isLargeNoIcon>
-                  Konsultasi
-                </Button>
+        <div className="row justify-content-center">
+          <div className="col-lg-6 col-sm-8">
+              <div className="contact-wrap justify-content-between">
+                <div className="contact-content">
+                  <h3>layanan berkualitas tinggi <br /> untuk masyarakat indonesia</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                  <Button isLargeColored className="mr-3 mb-3">
+                    Kontak Kami
+                  </Button>
+                  <Button isLargeNoIcon>
+                    Konsultasi
+                  </Button>
+                </div>
+                <div className="contact-img">
+                  <img src={ContactImg} alt="da" />
+                </div>
               </div>
-              <div className="contact-img">
-                <img src={ContactImg} alt="da" />
-              </div>
-            </div>
+          </div>
         </div>
-      </div>
     </section>
   )
 }
